7c970224a2c4278296ec765e7ef75440d2ea5a83
Arman Augusto <arman.augusto@gmail.com>
2019-02-07 15:30:57 -0800

    complete full application with edited comments extended

--- ReadyQueue.h
+++ ReadyQueue.h
@@ -1,15 +1,9 @@
-/*
- * Project Name:    Process Control Block
- * Description:     This is the header file for the Ready Queue
- *                  containing all processes in the "READY" state
- */
-
 /* 
  * File:            ReadyQueue.h
  * Author:          Arman Augusto
- * ID:              004588816
- * Class:           CS 433 - Operating Systems
- * Assignment:      Assignment 1
+ * Project Name:    Process Control Block
+ * Description:     This is the header file for the Ready Queue
+ *                  containing all processes in the "READY" state
  *
  * Created on January 30, 2019, 11:29 AM
  */
@@ -23,22 +17,22 @@
 
 class ReadyQueue {
 protected:
-    PCB queue[10]; // the queue of processes
-    int endOfQueue;    // represents the last index of the queue
+    PCB queue[10];  // the queue of processes
+    int endOfQueue; // represents the last index of the queue
 public:
-    //Error handling classes
+    // Error handling classes
     class Underflow{};    // no processes available
     class Overflow{};     // queue overflow
     class Illegal{};      // invalid priority value
     ReadyQueue();
     ~ReadyQueue();
-    void insertProc(PCB newProcess);      // adds the PCB of a process into the ReadyQueue. state changes to READY
+    void insertProc(PCB newProcess);            // adds the PCB of a process into the ReadyQueue. state changes to READY
     void removeHighestProc(int &returnValue);   // removes and returns the PCB with the highest priority from the queue. state changes to RUNNING
-    void reheapify();                         // reorders ReadyQueue placing highest priority process in front of queue
-    void age();                               // tracks how long processes remain in ReadyQueue
-    int size();                               // returns number of elements in the queue
-    void displayQueue();                      //Display the IDs and priorities of processes in the queue
-    bool isEven(int slot);                    //Checks if a number is even or odd for the purpose of reordering the ReadyQueue
+    void reorder();                             // reorders ReadyQueue placing highest priority process in front of queue
+    void age();                                 // tracks how long processes remain in ReadyQueue
+    int size();                                 // returns number of elements in the queue
+    void displayQueue();                        // display the IDs and priorities of processes in the queue
+    bool isEven(int slot);                      // checks if a number is even or odd for the purpose of reordering the ReadyQueue
 };
 
 #endif /* TESTREADYQUEUE_H */
